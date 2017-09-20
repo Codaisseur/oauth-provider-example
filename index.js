0053415ffa58059c8198e33e6238354727735d35
Wouter de Vos <wouter@codaisseur.com>
2017-09-20 15:35:54 +0200

    Set up Express

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const static = require('serve-static');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cors = require('cors');
+const session = require('express-session');
+const flash = require('express-flash');
+const models = require('./models');
+
+const config = require('config');
+
+const port = config.get('port');
+const secret = config.get('secret');
+
+const app = express()
+
+app
+  // Help secure Express apps with various HTTP headers.
+  // See: https://helmetjs.github.io/
+  .use(helmet())
+  .use(helmet.noCache())
+  .use(helmet.hsts({
+    maxAge: 31536000,
+    includeSubdomains: true
+  }))
+
+  // Static files
+  .use(favicon('./public/favicon.ico'))
+  .use(static('./public'))
+
+  // Enable CORS for all routes and origins
+  .use(cors(config.get('cors')))
+
+  // Set up cookie and body parsers
+  .use(cookieParser())
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+
+  // Set session support
+  .use(session(({ secret: secret, resave: true, saveUninitialized: true })))
+
+  // Flash messages support
+  .use(flash())
+
+  .get('/', (req, res) => {
+    res.json({ message: 'Hello, class' })
+  })
+
+  .listen(port);
